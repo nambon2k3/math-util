4ac1e36c8edb277cc79be6ff790ad4d12062b948
nambon2k3 <nambon2k3@gmail.com>
2023-08-21 16:44:46 +0700

    modify README.md and refeactor the getFactorial function

--- src/com/nambon/mathutil/core/MathUtil.java
+++ src/com/nambon/mathutil/core/MathUtil.java
@@ -12,20 +12,30 @@ public class MathUtil {
     //trong class nay cung cap cho ta rat nhieu ham xu li toan hoc
     //clone class Math cua JDK
     //ham thu vien xai chung cho ai do ma khong can luu lai trang thai/gia tri ==> dung ham static
-    
+
     //ham tinh giai thua
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0 and 20");
+//        }
+//        if(n == 1 || n == 0) {
+//            return 1;
+//        }
+//        
+//        long product = 1;
+//        for (int i = 2; i < n + 1; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    //dung de quy
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid argument. N must be between 0 and 20");
         }
-        if(n == 1 || n == 0) {
+        if (n == 1 || n == 0) {
             return 1;
         }
-        
-        long product = 1;
-        for (int i = 2; i < n + 1; i++) {
-            product *= i;
-        }
-        return product;
+        return n * getFactorial(n - 1);
     }
 }
